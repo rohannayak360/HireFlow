e632821526e61344687ac1f6055fe5247db7137b
Rohan Nayak <rohannayak360@gmail.com>
2026-06-29 17:56:25 +0530

    Renamed index-3.blade.php to home.blade.php and updated routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('frontend.index-3');
+    return view('frontend.home');
 });
 
 Route::prefix('admin')->group(function () {
