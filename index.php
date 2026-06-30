51c85b2d9c73776c5e74027ec78fec283abc387a
Rohan Nayak <rohannayak360@gmail.com>
2026-06-30 12:18:04 +0530

    Fixed missing CSS assets and updated root URL routing

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+// This file allows us to run Laravel from the root directory 
+// without using the "public" segment in the URL.
+require_once __DIR__.'/public/index.php';
